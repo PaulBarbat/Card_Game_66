e3f4cc04ad0fd9521dc3ffb47b1d109db82089c9
Paul Barbat <Paul.Barbat@Siriusxm.com>
2025-06-18 10:17:53 +0300

    Fixed Card class not being commited.

--- src/Card.cpp
+++ src/Card.cpp
@@ -1,3 +1,64 @@
 #include "Card.hpp"
 
-Card::Card(Suit suit, Rank rank);
+Card::Card(Suit suit, Rank rank): suit(suit), rank(rank) {}
+
+std::string Card::toString()
+{
+    std::string card="";
+    switch (this->rank)
+    {
+        case Rank::Two:
+        {
+            card +="Two";
+            break;
+        }
+        case Rank::Three:
+        {
+            card +="Three";
+            break;
+        }
+        case Rank::Four:
+        {
+            card +="Four";
+            break;
+        }
+        case Rank::Ten:
+        {
+            card +="Ten";
+            break;
+        }
+        case Rank::Ace:
+        {
+            card +="Ace";
+            break;
+        }
+        default:
+            break;
+    }
+    switch (this->suit)
+    {
+        case Suit::Hearts:
+        {
+            card+=" of Hearts";
+            break;
+        }
+        case Suit::Diamonds:
+        {
+            card+="of Diamonds";
+            break;
+        }
+        case Suit::Clubs:
+        {
+            card+="of Clubs";
+            break;
+        }
+        case Suit::Spades:
+        {
+            card+="of Spades";
+            break;
+        }
+        default:
+            break;
+    }
+    return card;
+}

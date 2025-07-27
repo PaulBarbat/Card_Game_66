5f451895aa41f79f7a60002ac0458527d03c84ce
Paul Barbat <Paul.Barbat@Siriusxm.com>
2025-07-27 20:42:02 +0300

    try with includes changes

--- CardViewer.cpp
+++ CardViewer.cpp
@@ -1,7 +1,7 @@
 
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <iostream>
 #include <string>
 

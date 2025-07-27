28ec769b156e0507af52e33aa469127f92a518f7
Paul Barbat <Paul.Barbat@Siriusxm.com>
2025-07-27 15:31:16 +0300

    New modifications for cmake

--- Toolchain_Windows.cmake
+++ Toolchain_Windows.cmake
@@ -17,21 +17,7 @@ set(SDL2_PATH        "${SDL2_ROOT}/SDL2-devel-2.32.8-mingw/SDL2-2.32.8/x86_64-w6
 set(SDL2_IMAGE_PATH  "${SDL2_ROOT}/SDL2_image-devel-2.8.8-mingw/SDL2_image-2.8.8/x86_64-w64-mingw32")
 set(SDL2_TTF_PATH    "${SDL2_ROOT}/SDL2_ttf-devel-2.24.0-mingw/SDL2_ttf-2.24.0/x86_64-w64-mingw32")
 
-# Include paths (for #include <SDL2/...>)
-include_directories(
-    ${SDL2_PATH}/include/SDL2
-    ${SDL2_IMAGE_PATH}/include/SDL2
-    ${SDL2_TTF_PATH}/include/SDL2
-)
-
-# Library directories
-link_directories(
-    ${SDL2_PATH}/lib
-    ${SDL2_IMAGE_PATH}/lib
-    ${SDL2_TTF_PATH}/lib
-)
-
-# Help CMake find things during cross-compilation
+# Help CMake find libraries and includes during cross-compilation
 set(CMAKE_FIND_ROOT_PATH ${SDL2_ROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
